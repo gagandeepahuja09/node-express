fa872d892ae21c60fd47478a5b2ca49c5f77fb32
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-18 17:37:45 +0530

    setting up express server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+// next is for middlewares
+
+app.use((req, res, next) => {
+	console.log(req.headers);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<html><body>This is an express server</body></html>')
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+	console.log(`Listening to port http://${hostname}:${port}`);
+});
